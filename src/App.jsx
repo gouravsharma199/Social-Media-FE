3e16b8aa53d1959f12d81c0cd5bd5b86fdd05e13
gouravsharma199 <gouravsharma7227199@gmail.com>
2026-07-16 11:35:51 +0530

    Complete Routing and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import myImage from "./assets/gourav.jpg";
+import NavBar from "./NavBar";
+import React from "react";
+import {BrowserRouter,Route,Routes} from "react-router-dom";
+import Profile from "./Profile";
+import Login from "./Login"
+import Body from "./Body";
+
+
+
 
 function App() {
   return (
-    <div className="navbar bg-base-300 shadow-sm">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="flex gap-2">
-    {/* <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" /> */}
-    <div className="dropdown dropdown-end mx-5">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img src={myImage} alt="Gourav" />
-        </div>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
+  <>
+    <BrowserRouter basename = "/">
+      <Routes>
+        <Route path="/" element={<Body/>}>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/profile" element={<Profile/>} ></Route>
+        </Route>
+         
+      </Routes>
+    </BrowserRouter>
+    
+  </>
+    
   );
 }
 
