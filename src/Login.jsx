024e07be905db428dd44cae78edeceb47dd7bcc7
gouravsharma199 <gouravsharma7227199@gmail.com>
2026-07-18 21:24:35 +0530

    install axios and use

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,27 +1,47 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios';
 const Login = () => {
+
+    const[email,setEmail] = useState(" ");
+    const[password,setPassword] = useState("");
+
+    const handleLogin = async()=>{
+        try{
+            const res = await axios.post("http://localhost:222/login",{
+                emailId: "Gourav@gmail.com",
+                password: "Gourav@321"
+            })
+
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+
+
   return (
     <div className="hero  max-h-screen my-10">
         <div className="hero-content flex-col lg:flex-row-reverse">
-            {/* <div className="text-center lg:text-left">
-               
-                <p className="py-6">
-                    login and start your jurney Today 
-                </p>
-            </div> */}
             <div className="card bg-base-300 w-full max-w-lg shrink-0 shadow-2xl p-10 m-4">
-            <div className="card-body">
-                 <h1 className="text-5xl font-bold mb-3"> Login now! </h1>
-                <fieldset className="fieldset">
-                <label className="label">Email</label>
-                <input type="email" className="input" placeholder="Email" />
-                <label className="label">Password</label>
-                <input type="password" className="input" placeholder="Password" />
-                <div><a className="link link-hover">Forgot password?</a></div>
-                <button className="btn btn-neutral mt-4">Login</button>
-                </fieldset>
-            </div>
+                <div className="card-body">
+                    <h1 className="text-5xl font-bold mb-3"> Login now! </h1>
+
+                    <fieldset className="fieldset">
+
+                        <label className="label">Email</label>
+                        <input type="email" value={email} className="input" 
+                        placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
+
+                        <label className="label">Password</label>
+                        <input type="password" className="input" value={password} 
+                        onChange={(e)=>setPassword(e.target.value)} placeholder="Password" />
+
+                        <div><a className="link link-hover">Forgot password?</a></div>
+
+
+                    <button className="btn btn-neutral mt-4">Login</button>
+                    </fieldset>
+                </div>
             </div>
         </div>
     </div>
